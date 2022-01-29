677ae2876a630ddddaa365e0be75feeea0e2d695
Sainath Machunuru <machunursainathgoud@gmail.com>
2022-01-29 23:58:59 +0530

    Add sample spring cloud server and client

---
+++ catalog-service-client/src/main/java/com/msng/catalogserviceclient/CatalogServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.msng.catalogserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceClientApplication.class, args);
+	}
+
+}
---
+++ catalog-service-client/src/test/java/com/msng/catalogserviceclient/CatalogServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msng.catalogserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
